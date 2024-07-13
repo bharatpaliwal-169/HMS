686488d4e93ae36d2cde151c7d2145d352d70fc4
Bharat Paliwal <18uec169@lnmiit.ac.in>
2024-07-13 23:08:52 +0530

    corrections and deploy warn remover

--- app/patients/[userId]/new-appointment/page.tsx
+++ app/patients/[userId]/new-appointment/page.tsx
@@ -12,7 +12,7 @@ const Appointment = async ({ params: { userId } }: SearchParamProps) => {
       <section className="remove-scrollbar container my-auto">
         <div className="sub-container max-w-[860px] flex-1 justify-between">
           <Image
-            src="/assets/icons/logo-full.svg"
+            src="/assets/icons/logo-nbg.svg"
             height={1000}
             width={1000}
             alt="logo"
